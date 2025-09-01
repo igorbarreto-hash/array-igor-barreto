540cbdfb80f07062762120fecb3316715522b09d
igorbarreto-hash <igorbarreto2908@gmail.com>
2025-09-01 19:55:07 -0300

    Adicionar solução

---
+++ array.py
@@ -0,0 +1,10 @@
+listaAlunos = ["Mauro", "Ricardo", "Amanda", "Roni", "Thamirys", "Carla"]
+carla = "Carla"
+
+print("Primeiro Elemento", listaAlunos[0])
+print("Terceiro Elemento", listaAlunos[2])
+print("Ultimo Elemento", listaAlunos[4])
+if carla in listaAlunos:
+    print("Carla está na Lista!")
+else:
+    print("Carla não foi encontrada na lista.")
